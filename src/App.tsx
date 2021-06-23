5fe090310360194a3f5bdcd2203b4d93550fa14c
Konstantin Shchygolev <konstantinshchygolev@Konstantins-MacBook-Pro.local>
2021-06-23 10:11:30 +0300

    fatched data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+
 const App: React.FunctionComponent = () => {
+  const [data, setData] = useState(undefined);
+
+  const fetchData = async () => {
+    const result = await fetch('https://api.covid19api.com/summary');
+    const data = await result.json();
+
+    setData(data);
+    console.log(data);
+    
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
-      
+      <h1>Global Covid-19 App</h1>
     </div>
   );
 }
